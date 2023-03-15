e54dc43f9510c342ee5caf6e2646e447feafe12e
Eddie <eddiemannan@gmail.com>
2023-03-15 14:01:31 -0400

    Added first two files

---
+++ src/01Printing.py
@@ -0,0 +1,16 @@
+###############################################################################
+#   Read the code below.  Predict (in your head)
+#   what will get printed (i.e., displayed) when the code runs.
+#
+#   Then run this module by clicking the play button in the top right
+#
+#   Confirm that you see the output in the  "Terminal"  window that appears.
+###############################################################################
+
+
+print('Hello, World')
+print('hi there')
+print('one', 'two', 'three')
+
+print(3 + 9)
+print('3 + 9', 'versus', 3 + 9)
---
+++ src/02MorePrinting.py
@@ -0,0 +1,73 @@
+###############################################################################
+#
+#   This line is a COMMENT -- a note to human readers of this file.
+#   When the Python interpreter RUNS (i.e. EXECUTES) a program, it ignores
+#   everything from a  #  (hash) mark to the end of the line with the  #  mark.
+#
+#   We call files that have Python code in them MODULES.
+#   Line 15 of this module (look at it now) prints (displays) the STRING
+#      Hello, World
+#   on the Run window.
+#
+#   Anything surrounded by quote marks (single or double) is a STRING.
+#
+###############################################################################
+print('Hello, World')
+###############################################################################
+#
+# TODO: 1.
+#   (Yes, that means for YOU to DO things per these instructions:)
+#
+#   Run this module by clicking the play button in the top right
+#
+#   After running, find the Run window and confirm that
+#        Hello, World
+#   did indeed get printed (displayed) on that window.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 2.
+#   When you have completed a TO-DO, you should change the word
+#          _TODO_      (ignore the underscores on this line)
+#        to
+#          DONE
+#        Try it now on line 19 above.
+#
+#   If you change each _TODO_ to DONE like this, you can tell when you
+#   have finished all the exercises in a module.
+#
+#   You have now completed _TODO_ #2, so change its _TODO_ on line 35 to DONE.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 3.
+#   Add another   print   statement below this comment.
+#   It should print any string that you want (but keep it polite and G-rated!)
+#
+#   Test your code by re-running this module.
+#   Look at the  Terminal  window to be sure that your string printed as expected.
+#
+###############################################################################
+
+# PUT IT HERE
+
+###############################################################################
+#
+# TODO: 4.
+#   Add yet another   print   statement, putting it below this comment.
+#   This one should print the *product* of 3,607 and 34,227.
+#   Let the computer do the arithmetic for you (no calculators!).
+#   You do NOT have to use strings for this, so no quotation marks!
+#
+#   TEST your code by re-running this module, then asking someone
+#   whom you trust:
+#      What number did your   print   display for this TO-DO?
+#   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
+#
+###############################################################################
+
+# PUT IT HERE
